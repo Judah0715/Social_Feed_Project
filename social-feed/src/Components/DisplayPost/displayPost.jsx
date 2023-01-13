181df17329e074efe8373974828b02b40f30b1e3
Judah <judah.agnew121@gmail.com>
2023-01-13 13:45:36 -0800

    Created Displaypost Folder + Styling

---
+++ social-feed/src/Components/DisplayPost/displayPost.jsx
@@ -0,0 +1,33 @@
+import LikeDislikeButton from "../Buttons/Buttons";
+
+
+
+const DisplayPost = (props) => {
+    return (  
+        <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Post</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry,index) => {
+            return (
+              <tr key={index}>
+                <td>{entry.name}</td>
+                <li>
+                <td>{entry.post}</td>
+                </li>
+                <td>{entry.date}</td>
+                <LikeDislikeButton />
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+}
+ 
+export default DisplayPost;
